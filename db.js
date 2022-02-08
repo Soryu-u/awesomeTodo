b3f26076ff2d7f0a24b5fa714f46f33b0d2d9de7
Soryu <53074697+Soryu-u@users.noreply.github.com>
2022-02-08 19:51:07 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const knex = require("knex")({
   client: "pg",
   connection: {
-    user: "szpaku",
-    password: "Windranger123",
-    host: "localhost",
-    post: 5432,
-    database: "incamp",
+    user: "<username>",
+    password: "<password>",
+    host: "<host>",
+    post: <port>,
+    database: "<databaseName>",
   },
 });
 
